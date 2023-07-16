da0f8202860621e66990a49e709c425931a91795
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-07-16 16:33:26 -0300

    Adicionando a lista de tarefas iniciais ao projeto

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
+  
+  return <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className='todo-list'> 
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+}
+
+export default App
