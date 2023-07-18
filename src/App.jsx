c98160f3142340a504388f4f6c8b2ea749fa1d39
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-07-18 16:33:46 -0300

    Adicionando ordenação por ordem alfabética na pesquisa das tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import TodoForm from "./components/TodoForm";
 import Search from "./components/Search";
 import Todo from "./components/todo";
+import Filter from "./components/Filter";
 
 import "./App.css";
 
@@ -30,6 +31,9 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -64,10 +68,23 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
           )
           .map((todo) => (
             <Todo
