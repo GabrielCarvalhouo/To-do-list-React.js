0fbaeaa53a7f8ff2ab2582d34acec8b1ed87e363
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-07-18 13:11:13 -0300

    Adicionando os botões de tarefas finalizadas e remover tarefas

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Todo = ({todo}) => {
+const Todo = ({todo, removeTodo, completeTodo}) => {
   return (
-    <div className="todo">
+    <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
             <div className="content">
               <p>{todo.text}</p>
               <p className="category">({todo.category})</p>
             </div>
             <div>
-              <button className='complete'>Completar</button>
-              <button className='remove'>x</button>
+              <button className='complete' onClick={() => completeTodo(todo.id)} >Completar</button>
+              <button className='remove' onClick={() => removeTodo(todo.id)}>x</button>
             </div>
           </div>
   )
